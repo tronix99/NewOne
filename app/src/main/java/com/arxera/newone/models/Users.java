ed9cf9ced1171ad50d92c378dfe56fb9983168cd
Rahul <rsulegaokar@gmail.com>
2018-01-15 16:25:07 +0530

    Login With Google Firebase

---
+++ app/src/main/java/com/arxera/newone/models/Users.java
@@ -0,0 +1,58 @@
+package com.arxera.newone.models;
+
+/**
+ * Created by Rahul on 1/15/2018.
+ */
+
+public class Users {
+    private String user;
+    private String email;
+    private String photUrl;
+    private String Uid;
+
+    public Users() {
+    }
+
+    public Users(String user) {
+        this.user = user;
+    }
+
+    public Users(String user, String email, String photUrl, String uid) {
+        this.user = user;
+        this.email = email;
+        this.photUrl = photUrl;
+        Uid = uid;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhotUrl() {
+        return photUrl;
+    }
+
+    public void setPhotUrl(String photUrl) {
+        this.photUrl = photUrl;
+    }
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+}
---
+++ app/src/main/java/com/arxera/newone/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.arxera.newone.utils;
+
+/**
+ * Created by Rahul on 1/15/2018.
+ */
+public class Constants {
+    public static final String USER_KEY = "users";
+}
